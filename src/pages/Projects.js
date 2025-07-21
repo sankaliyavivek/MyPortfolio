93e45c10f19a7329c9450c39287c8a47c73efb3b
vivek <sankaliyavivek9797@gmail.com>
2025-07-21 17:28:57 +0530

    final

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,121 +1,102 @@
 import React from 'react';
 import './Projects.css';
 import ProjectCard from '../components/ProjectCard';
-import Raajsolar from '../assets/RAAJ-SOLAR.png';
-import Siddhguruimg from '../assets/SIDDHGURU.png';
-import Portfolioimg from '../assets/portfolio.png';
-import Roayltoucheimg from '../assets/royaltouche.png';
-import PvotIMg from '../assets/pvotimg.png';
-import Interiormaata from '../assets/Interiorimg.png';
-import Building from "../assets/image.png";
-import Building1 from "../assets/image2.png";
-import Building2 from "../assets/image3.png";
-import crown from "../assets/image4.png";
-import credai from "../assets/image5.png";
+// import Raajsolar from '../assets/RAAJ-SOLAR.png';
+// import Siddhguruimg from '../assets/SIDDHGURU.png';
+// import Portfolioimg from '../assets/portfolio.png';
+// import Roayltoucheimg from '../assets/royaltouche.png';
+// import PvotIMg from '../assets/pvotimg.png';
+// import Interiormaata from '../assets/Interiorimg.png';
+// import Building from "../assets/image.png";
+// import Building1 from "../assets/image2.png";
+// import Building2 from "../assets/image3.png";
+// import crown from "../assets/image4.png";
+// import credai from "../assets/image5.png";
+
+import Toys from '../assets/toys.png';
+import Ashin from '../assets/ashin.png';
+import Portfolio from '../assets/portfolio.png';
+import Game from '../assets/game.png';
 const Projects = () => {
   const projects = [
     {
       id: 1,
-      image_url: Roayltoucheimg,
-      project_name: 'Royale-Touche Website',
-      // description: 'Description of Project 1',
-      tech_stack:["NextJs"],
-      // source_link: 'https://royale-touche.vercel.app/'
-      source_link: 'https://plywood.royaletouche.com/'
-      // https://plywood.royaletouche.com/
+      image_url: Toys,
+      project_name: 'My-Toys',
+      tech_stack:["React","LocalStorage"],
+      source_link: 'https://mytoysone.vercel.app/'
     },
     {
       id: 2,
-      image_url: Siddhguruimg,
-      project_name: 'Siddhguru Website',
-      // description: 'My Personal Portfolio',
-      tech_stack: ['React',"Sass"],
-      source_link: 'https://www.sribrahmrishiashram.org/'
+      image_url: Game,
+      project_name: 'MyExclusive',
+      tech_stack: ['Html',"css","JS","Bootstrap"],
+      source_link: 'https://sankaliyavivek.github.io/game/'
     },
      {
       id: 3,
-      image_url: Raajsolar,
-      project_name: 'Raaj-Solar Website',
-      // description: 'Description of Project 2',
-      tech_stack: ['React', 'Sass'],
-      source_link: 'https://raajsolar.com/'
+      image_url: Portfolio,
+      project_name: 'My Personal Portfolio',
+      tech_stack: ['React'],
+      source_link: 'https://sankaliyavivek.github.io/MyPortfolio/'
     },
-          {
+      {
         id: 9,
-        image_url: Building2,
-        project_name: 'Agency Website Freelancing',
-        // description: 'Description of Project 2',
-        tech_stack: ['NextJs'],
+        image_url: Ashin,
+        project_name: 'Ashin Fashion',
+        tech_stack: ['Html',"css","JS"],
+        source_link: 'https://sankaliyavivek.github.io/Ashin/'
+      },
+      // {
+      //   id: 10,
+      //   image_url: "",
+      //   project_name: 'Royal Crown',
+      //   tech_stack: ['NextJs'],
+      //   source_link: 'https://royalcrownfont.vercel.app/'
+      // },
+    // {
+    //     id: 4,
+    //     image_url: "",
+    //     project_name: 'My Personal Portfolio',
+    //     tech_stack: ['React'],
+    //     source_link: 'https://jaykalariya.vercel.app/'
+    //   },
+      // {
+      //   id: 5,
+      //   image_url: "",
+      //   project_name: 'PVOT Website',
+      //   tech_stack: ['React'],
+      //   source_link: 'https://humbee-developers.github.io/PVOT/'
+      // },
+      // {
+      //   id: 6,
+      //   image_url: "",
+      //   project_name: 'InteriorMaata Website',
+      //   tech_stack: ['NextJs'],
+      //   source_link: 'https://interiormaata.com/'
+      // },
+      // {
+      //   id: 7,
+      //   image_url: "",
+      //   project_name: 'The Building Company',
+      //   tech_stack: ['HTML CSS JS'],
+      //   source_link: 'https://thebuildingcompany.vercel.app/'
+      // },
+      //    {
+      //   id: 8,
+      //   image_url: "",
+      //   project_name: 'Website Template',
+      //   tech_stack: ['HTML CSS JS'],
+      //   source_link: 'https://website-template-teal.vercel.app/'
+      // },
+      //  {
+      //   id: 11,
+      //   image_url: "",
+      //   project_name: 'Credai Rera',
+      //   tech_stack: ['HTML CSS JS'],
         
-        source_link: 'https://roardata.com.au/'
-        // source_link: 'https://humbee-developers.github.io/InteriorMaata-/'
-      },
-              {
-        id: 10,
-        image_url: crown,
-        project_name: 'Royal Crown',
-        // description: 'Description of Project 2',
-        tech_stack: ['NextJs'],
-        
-        source_link: 'https://royalcrownfont.vercel.app/'
-        // source_link: 'https://humbee-developers.github.io/InteriorMaata-/'
-      },
-    {
-        id: 4,
-        image_url: Portfolioimg,
-        project_name: 'My Personal Portfolio',
-        // description: 'Description of Project 2',
-        tech_stack: ['React'],
-        source_link: 'https://jaykalariya.vercel.app/'
-      },
-      {
-        id: 5,
-        image_url: PvotIMg,
-        project_name: 'PVOT Website',
-        // description: 'Description of Project 2',
-        tech_stack: ['React'],
-        source_link: 'https://humbee-developers.github.io/PVOT/'
-      },
-      {
-        id: 6,
-        image_url: Interiormaata,
-        project_name: 'InteriorMaata Website',
-        // description: 'Description of Project 2',
-        tech_stack: ['NextJs'],
-        
-        source_link: 'https://interiormaata.com/'
-        // source_link: 'https://humbee-developers.github.io/InteriorMaata-/'
-      },
-      {
-        id: 7,
-        image_url: Building,
-        project_name: 'The Building Company',
-        // description: 'Description of Project 2',
-        tech_stack: ['HTML CSS JS'],
-        
-        source_link: 'https://thebuildingcompany.vercel.app/'
-        // source_link: 'https://humbee-developers.github.io/InteriorMaata-/'
-      },
-         {
-        id: 8,
-        image_url: Building1,
-        project_name: 'Website Template',
-        // description: 'Description of Project 2',
-        tech_stack: ['HTML CSS JS'],
-        
-        source_link: 'https://website-template-teal.vercel.app/'
-        // source_link: 'https://humbee-developers.github.io/InteriorMaata-/'
-      },
-       {
-        id: 11,
-        image_url: credai,
-        project_name: 'Credai Rera',
-        // description: 'Description of Project 2',
-        tech_stack: ['HTML CSS JS'],
-        
-        source_link: 'https://reraarea.com/'
-        // source_link: 'https://humbee-developers.github.io/InteriorMaata-/'
-      },
+      //   source_link: 'https://reraarea.com/'
+      // },
   ];
 
   return (
